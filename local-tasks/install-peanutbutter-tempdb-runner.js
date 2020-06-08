53c4d8ee8fcda09d3f9e9d58a7d99d1c87342b80
Davyd McColl <davydm@gmail.com>
2020-06-08 19:56:03 +0200

    :alembic: attempt GH test passing with node-nuget-client

--- local-tasks/install-peanutbutter-tempdb-runner.js
+++
@@ -1,7 +0,0 @@
-const
-  spawnNuget = requireModule("spawn-nuget"),
-  gulp = requireModule("gulp-with-help");
-
-gulp.task("install-peanutbutter-tempdb-runner", async () => {
-  return spawnNuget([ "install", "PeanutButter.TempDb.Runner", "-Version", "1.2.362" ]);
-})
