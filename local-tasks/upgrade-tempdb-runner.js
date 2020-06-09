e55c2b7c776e1523219bf8c079b7e5a050474be9
Davyd McColl <davydm@gmail.com>
2020-06-09 20:39:01 +0200

    f

--- local-tasks/upgrade-tempdb-runner.js
+++ local-tasks/upgrade-tempdb-runner.js
@@ -1,5 +1,6 @@
 const
     rimraf = requireModule("rimraf"),
+    chalk = require("chalk"),
     readPackageJson = requireModule("read-package-json"),
     writeTextFile = requireModule("write-text-file"),
     { NugetClient } = require("node-nuget-client"),
@@ -12,19 +13,41 @@ gulp.task("upgrade-tempdb-runner", async () => {
         output = path.dirname(__dirname),
         nuget = new NugetClient();
 
-    await rimraf(path.join(output, `${tempDbPackageName}*`));
-
-    const dlResult = await nuget.downloadPackage({
-        packageId: tempDbPackageName,
-        output
-    })
-
-    await updatePackageFiles();
-    // update postinstall script
-    const index = await readPackageJson();
-    index.scripts.postinstall = `node-nuget download ${dlResult.fullName}`;
-    await writeTextFile(
-        "package.json",
-        JSON.stringify(index)
+    await run(
+        `remove any existing ${ tempDbPackageName }`,
+        () => rimraf(path.join(output, `${ tempDbPackageName }*`))
     );
+
+    const dlResult = await run(
+        `download latest ${ tempDbPackageName }`,
+        () => nuget.downloadPackage({
+            packageId: tempDbPackageName,
+            output
+        })
+    );
+
+    await run(`Update package files entry`, updatePackageFiles);
+    // update postinstall script
+    await run(`Update postinstall script`,
+        async () => {
+            const index = await readPackageJson();
+            index.scripts.postinstall = `node-nuget download ${ dlResult.fullName }`;
+            await writeTextFile(
+                "package.json",
+                JSON.stringify(index, null, 2)
+            );
+        });
 });
+
+async function run(label, action) {
+    try {
+        process.stdout.write(`${ chalk.yellow("[ -- ]") } ${ label }`);
+        process.stdout.flush();
+        const result = await action();
+        console.log((`\r${ chalk.green("[ OK ]") } ${ label }`));
+        return result;
+    } catch (e) {
+        console.log((`\r${ chalk.red("[FAIL]") } ${ label }`));
+    }
+}
+
